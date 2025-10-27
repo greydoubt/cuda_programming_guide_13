b1c865db784ce538b82ef36e56b1118edaed90a4
greydoubt <43443470+greydoubt@users.noreply.github.com>
2025-10-27 01:35:49 -0700

    Create 1_hello_world.cu

---
+++ 1_hello_world.cu
@@ -0,0 +1,21 @@
+#include <cuda_runtime.h>
+#include <stdio.h>
+
+void helloCPU()
+{
+  printf("Hello from the CPU.\n");
+}
+
+__global__ void helloGPU()
+{
+  printf("Hello from the GPU.\n");
+}
+
+int main()
+{
+  helloCPU();
+  
+  helloGPU<<<1, 1>>>();
+  
+  cudaDeviceSynchronize();
+}

8696f3177abf2db77c08438c5d42da318c07b8ca
greydoubt <43443470+greydoubt@users.noreply.github.com>
2025-10-27 18:12:53 -0700

    Update 2_basic_device_kernel.cu

--- 2_basic_device_kernel.cu
+++ 2_basic_device_kernel.cu
@@ -1,1 +1,27 @@
+#include <cuda_runtime.h>
+#include <stdio.h>
 
+__global__ void device_kernel_add() {
+  printf("Does add on device");
+}
+
+__global__ void device_kernel_mul() {
+  printf("Does mul on device");
+}
+
+void host_func_add() {
+  printf("Does add on host");
+}
+
+void host_func_mul() {
+  printf("Does mul on host");
+}
+
+int main()
+{
+  helloCPU();
+  
+  helloGPU<<<1, 1>>>();
+  
+  cudaDeviceSynchronize();
+}

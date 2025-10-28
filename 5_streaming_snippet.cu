ff41f9afeec00a5d10a035359ab59835b0a7b351
greydoubt <43443470+greydoubt@users.noreply.github.com>
2025-10-27 20:12:52 -0700

    Update 5_streaming_snippet.cu

--- 5_streaming_snippet.cu
+++ 5_streaming_snippet.cu
@@ -1,3 +1,23 @@
+#include <cuda_runtime.h>
+#include <stdio.h>
+
+__global__ void device_kernel_add() {
+  printf("Does add on device");
+}
+
+__global__ void device_kernel_mul() {
+  printf("Does mul on device");
+}
+
+void host_func_add() {
+  printf("Does add on host");
+}
+
+void host_func_mul() {
+  printf("Does mul on host");
+}
+
+
 cudaStream_t s_1, s_2;
 cudaStreamCreate(&s_1);
 cudaStreamCreate(&s_2);

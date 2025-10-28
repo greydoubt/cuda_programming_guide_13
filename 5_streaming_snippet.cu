7aaf693eac197b4d08e1f7f402d7315df52ef662
greydoubt <43443470+greydoubt@users.noreply.github.com>
2025-10-27 20:13:51 -0700

    Update 5_streaming_snippet.cu

--- 5_streaming_snippet.cu
+++ 5_streaming_snippet.cu
@@ -17,26 +17,38 @@ void host_func_mul() {
   printf("Does mul on host");
 }
 
+int main()
+{
+  //timer.start();
 
-cudaStream_t s_1, s_2;
-cudaStreamCreate(&s_1);
-cudaStreamCreate(&s_2);
+  uint64_t * host_ptr, * device_ptr;
 
-cudaMemcpyAsync(host_ptr, device_ptr, size, cudaMemcpyDeviceToHost, s_2);
-cudaMemcpyAsync(host_ptr, device_ptr, size, cudaMemcpyDeviceToHost, s_1);
+  const uint64_t size = 64;
 
-device_kernel_add<<<grid, block, 0, s_1>>>();
-device_kernel_add<<<grid, block, 0, s_2>>>();
-device_kernel_mul<<<grid, block, 0, s_1>>>();
-device_kernel_mul<<<grid, block, 0, s_2>>>();
+  int grid 1;
+  int block 1;
 
-cudaMemcpyAsync(device_ptr, host_ptr, size, cudaMemcpyHostToDevice, s_2);
-cudaMemcpyAsync(device_ptr, host_ptr, size, cudaMemcpyHostToDevice, s_1);
+  cudaStream_t s_1, s_2;
+  cudaStreamCreate(&s_1);
+  cudaStreamCreate(&s_2);
 
-cudaStreamSynchronize(s_1);
-cudaStreamSynchronize(s_2);
+  cudaMemcpyAsync(host_ptr, device_ptr, size, cudaMemcpyDeviceToHost, s_2);
+  cudaMemcpyAsync(host_ptr, device_ptr, size, cudaMemcpyDeviceToHost, s_1);
 
-cudaStreamDestroy(s_1);
-cudaStreamDestroy(s_2);
+  device_kernel_add<<<grid, block, 0, s_1>>>();
+  device_kernel_add<<<grid, block, 0, s_2>>>();
+  device_kernel_mul<<<grid, block, 0, s_1>>>();
+  device_kernel_mul<<<grid, block, 0, s_2>>>();
+
+  cudaMemcpyAsync(device_ptr, host_ptr, size, cudaMemcpyHostToDevice, s_2);
+  cudaMemcpyAsync(device_ptr, host_ptr, size, cudaMemcpyHostToDevice, s_1);
+
+  cudaStreamSynchronize(s_1);
+  cudaStreamSynchronize(s_2);
+
+  cudaStreamDestroy(s_1);
+  cudaStreamDestroy(s_2);
+
+}
 
 //custom created streams run concurrently (s_1 and s_2 run independently of each other), but tasks scheduled on each stream execute sequentially, respecting the order in which they were scheduled.
